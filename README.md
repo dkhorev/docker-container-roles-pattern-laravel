87043d1f628012930701c2ff5d7aa274179e9c4d
dkhorev <me@dkhorev.pro>
2022-08-23 16:38:09 -0400

    add demo job

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Demo: Docker container roles pattern for monolith Laravel apps
+
+### run Redis with persistence
+
+docker run -p 6379:6379 --name redis -d redis --save 60 1 --loglevel warning
+
+in the `.env` file change `QUEUE_CONNECTION` from `sync` to `redis`
---
+++ app/Jobs/AsyncJobDemo.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class AsyncJobDemo implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public function handle()
+    {
+        $now = now()->toJSON();
+        Log::info("Async job demo run: $now");
+    }
+}
