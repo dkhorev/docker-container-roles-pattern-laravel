29aab854c36680db9fdde89a02280f6e3c99f3eb
dkhorev <me@dkhorev.pro>
2022-08-24 10:13:35 -0400

    add basic migration demo

---
+++ database/migrations/2022_08_24_141244_demo_migration.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        // this will fail since no Mysql is running
+    }
+
+    public function down(): void
+    {
+        // this will fail since no Mysql is running
+    }
+};
